5d8deb12b927d3c3ae6e1965925436f330ad9413
aulia2424 <auliamartha243@gmail.com>
2022-10-05 12:32:48 +0700

    05-10-2022_mathOperator

---
+++ 7.1LessonsMathOperator.js
@@ -0,0 +1,26 @@
+//OPERATOR ARITMATIKA
+
+// operator penjumlahan
+var x = 10 + 5;
+console.log(x);
+
+//  Anda dapat menambahkan angka atau variabel sebanyak yang Anda inginkan atau butuhkan
+var y = 10;
+var y = x + 5 + 22 + 45 + 6548;
+console.log(y);
+
+// perkalian (*) multiplication
+var a = 10 * 5;
+console.log(a);
+
+// pembagian (/) division
+var b = 100 / 5;
+console.log(b);
+
+// sisa bagi (%) moodulus
+var myVariable = 26 % 6;
+console.log(myVariable);
+
+// increment++ Operator kenaikan meningkatkan nilai numerik operan dengan 1. Ketika ditempatkan sebelum operan, itu akan mengembalikan nilai yang bertambah. Ketika ditempatkan setelahnya, itu akan mengembalikan nilai asli dan kemudian menambah operan.
+
+// Decrement -- Operator penurunan menurunkan nilai numerik operan dengan 1. Ketika ditempatkan sebelum operan, itu akan mengembalikan nilai yang dikurangi. Ketika ditempatkan setelah operan, itu akan mengembalikan nilai asli dan kemudian mengurangi operan.
