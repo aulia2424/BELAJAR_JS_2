a938231a426980114c882795f47dc853ec26189e
aulia2424 <auliamartha243@gmail.com>
2022-10-05 12:43:04 +0700

    belajar loops

---
+++ loops.js
@@ -0,0 +1,13 @@
+for (k = 1; k <= 5; k++) {
+  console.log(k);
+}
+
+var j = 1;
+for (; j <= 5; j++) {
+  console.log(j);
+}
+
+for (i = 1, text = ""; i < 5; i++) {
+  text = i;
+  console.log(i);
+}
