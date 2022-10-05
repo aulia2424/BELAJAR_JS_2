71036aae237b2225a79adc87eaf61e57e1347dac
aulia2424 <auliamartha243@gmail.com>
2022-10-05 19:58:45 +0700

    add file 25.1FunctionParameters

---
+++ 25.1FunctionParameters.js
@@ -0,0 +1,9 @@
+// using parameters
+function sayHello(name) {
+  console.log("hi, " + name); //harusya alert, tapi tidak bisa di node js jadi memakai console.log
+}
+sayHello("David");
+sayHello("Aul");
+sayHello("Sarah");
+
+//
