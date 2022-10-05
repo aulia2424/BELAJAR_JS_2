5b3c6d6cdec1e57353df4f84e2da2a0ab5e19809
aulia2424 <auliamartha243@gmail.com>
2022-10-05 12:55:20 +0700

    belajar whil loop

---
+++ 18.1TheForLoop.js
@@ -0,0 +1,25 @@
+for (k = 1; k <= 5; k++) {
+  console.log(k);
+}
+
+var j = 1;
+for (; j <= 5; j++) {
+  console.log(j);
+}
+
+for (i = 1, text = ""; i < 5; i++) {
+  text = i;
+  console.log(i);
+}
+
+// FOR LOOP
+var l = 0;
+for (; l < 10; ) {
+  console.log(l);
+  l++;
+}
+
+var x = 0;
+for (; x <= 20; x += 2) {
+  console.log(x);
+}
---
+++ 19.1TheWhileLoop.js
@@ -0,0 +1,13 @@
+// The While Loop
+var i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// contoh 2
+var x = 1;
+while (x <= 5) {
+  console.log(x);
+  x = x + 1;
+}
