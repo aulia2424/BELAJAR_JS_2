704c08d8d096ae6488572cb8bdea1cc1e2b6c80b
aulia2424 <auliamartha243@gmail.com>
2022-10-05 20:28:27 +0700

    add file 33.1

---
+++ 33.1ObjectInitialization.js
@@ -0,0 +1,25 @@
+/*
+Object Initialization
+Use the object literal or initializer syntax to create single objects.
+*/
+var John = { name: "John", age: 25 };
+var James = { name: "James", age: 21 };
+
+// contoh 2
+simba = {
+  catogory: "lion",
+  gender: "male",
+};
+
+/*
+Using Object Initializers
+Spaces and line breaks are not important. An object definition can span multiple lines.
+*/
+var John = {
+  name: "John",
+  age: 25,
+};
+var James = {
+  name: "James",
+  age: 21,
+};
